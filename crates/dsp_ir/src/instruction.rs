88319313b7c9512cd13b8707c845fd5aa25497cf
Austin Hicks <ahicks@ahicks.io>
2022-06-18 14:45:57 -0700

    Add a bunch more instructions we're going to need in order to be able to do things like filter design

--- crates/dsp_ir/src/instruction.rs
+++ crates/dsp_ir/src/instruction.rs
@@ -1,4 +1,4 @@
-use crate::context::ValueRef;
+use crate::context::*;
 
 /// The instruction enum.
 ///
@@ -81,5 +81,78 @@ pub enum Instruction {
     FastTanh {
         output: ValueRef,
         input: ValueRef,
+    },
+
+    /// Read a state at a given index.
+    ///
+    /// Index should be an integral scalar.
+    ///
+    /// Note that scalars are 1-length buffers, and so said index is always zero.
+    ReadState {
+        output: ValueRef,
+        state: StateRef,
+        index: ValueRef,
+    },
+
+    /// Read a state, but relative to the modulus of the current sample index.
+    ///
+    /// This is used to efficiently implement ringbuffers: the current index is `sample % length`, and the read is
+    /// relative to that.  The difference between this instruction and an addition followed by a read is that the
+    /// counter is guaranteed to be shared.
+    ReadStateRelative {
+        output: ValueRef,
+        state: StateRef,
+        index: ValueRef,
+    },
+
+    /// Read the current time, in samples.
+    ReadTimeSamples {
+        output: ValueRef,
+    },
+
+    /// Read the current time, in seconds.
+    ReadTimeSeconds {
+        output: ValueRef,
+    },
+
+    /// Read an input of the program, at the current sample index.
+    ReadInput {
+        output: ValueRef,
+        index: usize,
+    },
+
+    /// Write an output of the current program.
+    WriteOutput {
+        input: ValueRef,
+        index: usize,
+    },
+
+    Min {
+        output: ValueRef,
+        left: ValueRef,
+        right: ValueRef,
+    },
+
+    Max {
+        output: ValueRef,
+        left: ValueRef,
+        right: ValueRef,
+    },
+
+    Clamp {
+        output: ValueRef,
+        input: ValueRef,
+        lower: ValueRef,
+        upper: ValueRef,
+    },
+
+    ToF32 {
+        input: ValueRef,
+        output: ValueRef,
+    },
+
+    ToF64 {
+        input: ValueRef,
+        output: ValueRef,
     },
 }
