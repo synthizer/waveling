e0b557cb9c4ca8aec28b51ab09421bf555a62f40
Austin Hicks <ahicks@ahicks.io>
2022-06-19 11:33:55 -0700

    dsp_ir: add methods to iterate over inputs, outputs, and properties

--- crates/dsp_ir/src/context/mod.rs
+++ crates/dsp_ir/src/context/mod.rs
@@ -12,6 +12,7 @@ use anyhow::Result;
 use generational_arena::Arena;
 
 use crate::constant::Constant;
+use crate::Type;
 
 #[derive(Default)]
 pub struct Context {
@@ -52,8 +53,24 @@ impl Context {
         self.inputs.get(index)
     }
 
+    pub fn get_num_inputs(&self) -> usize {
+        self.inputs.len()
+    }
+
+    pub fn iter_inputs(&self) -> impl Iterator<Item = (usize, &Type)> {
+        self.inputs.iter().enumerate()
+    }
+
     pub fn get_output_type(&self, index: usize) -> Option<&crate::Type> {
         self.outputs.get(index)
+    }
+
+    pub fn get_num_outputs(&self) -> usize {
+        self.properties.len()
+    }
+
+    pub fn iter_ooutputs(&self) -> impl Iterator<Item = (usize, &Type)> {
+        self.outputs.iter().enumerate()
     }
 
     /// Declare a property.
@@ -70,5 +87,13 @@ impl Context {
 
     pub fn get_property_type(&self, index: usize) -> Option<&crate::Type> {
         self.properties.get(index)
+    }
+
+    pub fn get_num_properties(&self) -> usize {
+        self.properties.len()
+    }
+
+    pub fn iter_properties(&self) -> impl Iterator<Item = (usize, &Type)> {
+        self.properties.iter().enumerate()
     }
 }
