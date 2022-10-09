1a9018dffc66d404c1eed20bc6cc9a0b4f472ac1
Austin Hicks <ahicks@ahicks.io>
2022-10-09 16:03:02 -0700

    Op: introduce OpDescriptor, and make operators self-describing

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code)]
 
-pub mod diagnostics;
 pub mod constant;
+pub mod diagnostics;
 pub mod edge;
 pub mod node;
 pub mod op;
+pub mod passes;
 pub mod program;
 pub mod source_loc;
 pub mod state;
-pub mod passes;
 pub mod vector_descriptor;
 
 pub use crate::constant::*;
@@ -16,8 +16,8 @@ pub use diagnostics::*;
 pub use edge::*;
 pub use node::*;
 pub use op::*;
+pub use passes::*;
 pub use program::*;
 pub use source_loc::*;
 pub use state::*;
-pub use passes::*;
 pub use vector_descriptor::*;
--- crates/core/src/op.rs
+++ crates/core/src/op.rs
@@ -1,7 +1,9 @@
+use std::borrow::Cow;
+
 use crate::{Constant, PrimitiveType};
 
 /// Binary operations that we support.
-#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, derive_more::IsVariant)]
 pub enum BinOp {
     Add,
     Sub,
@@ -10,11 +12,12 @@ pub enum BinOp {
 }
 
 /// Kinds of operation associated with a node.
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, PartialEq, PartialOrd, derive_more::IsVariant)]
 pub enum Op {
     Constant(Constant),
 
     Negate,
+
     BinOp(BinOp),
 
     /// Read the given input.
@@ -51,6 +54,9 @@ pub enum Op {
     /// Read the clock, an i64 integer that increments every sample.
     Clock,
 
+    /// Read the sample rate.
+    Sr,
+
     /// Cast the only input to the given primitive type.
     ///
     /// We don't perform implicit casts because it is important to always know where they happen.
@@ -68,4 +74,98 @@ pub enum Op {
     /// This gives us a place to hook side-effecting operations which are not related to outputs, for example writing
     /// states.
     Final,
+}
+
+/// A descriptor for an operation, which describes the inputs and outputs for the type checker and opptimization passes.
+#[derive(Clone, Debug)]
+pub struct OpDescriptor {
+    /// Is this operator commutative?
+    ///
+    /// If so, then `a op b == b op a`.  We use a relaxed model that assumes fp ops are commutative.
+    pub commutative: bool,
+
+    pub inputs: Cow<'static, [InputDescriptor]>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, Hash, derive_more::IsVariant)]
+enum InputKind {
+    /// This edge carries data.
+    ///
+    /// Edges which carry data must type check.
+    Data,
+
+    /// This edge is a pure dependency.
+    ///
+    /// these are used to, for example, connect everything that needs to be connected to the final node.  Such edges
+    /// don't care about types.
+    PureDependency,
+}
+#[derive(Clone, Debug)]
+pub struct InputDescriptor {
+    input_kind: InputKind,
+
+    /// We assume all primitive types are allowed unless otherwise specified; this is the list of denied primitive
+    /// types.
+    ///
+    /// For example, we can't apply arithmetic binary operations to booleans.
+    denied_primitives: Option<Cow<'static, [PrimitiveType]>>,
+}
+
+fn binop_to_descriptor(o: BinOp) -> OpDescriptor {
+    OpDescriptor {
+        commutative: [BinOp::Add, BinOp::Mul].contains(&o),
+        inputs: Cow::Borrowed(&[InputDescriptor {
+            input_kind: InputKind::Data,
+            denied_primitives: Some(Cow::Borrowed(&[PrimitiveType::Bool])),
+        }]),
+    }
+}
+
+impl Op {
+    pub fn get_descriptor(&self) -> Cow<'static, OpDescriptor> {
+        match *self {
+            // All of our boring ones with no inputs.
+            Op::Start
+            | Op::ReadInput(_)
+            | Op::ReadProperty(_)
+            | Op::Constant(_)
+            | Op::ReadState { .. }
+            | Op::Clock
+            | Op::Sr => Cow::Borrowed(&OpDescriptor {
+                commutative: false,
+                inputs: Cow::Borrowed(&[]),
+            }),
+            Op::BinOp(o) => Cow::Owned(binop_to_descriptor(o)),
+            Op::Negate => Cow::Owned(OpDescriptor {
+                commutative: false,
+                inputs: Cow::Borrowed(&[InputDescriptor {
+                    input_kind: InputKind::Data,
+                    denied_primitives: Some(Cow::Borrowed(&[PrimitiveType::Bool])),
+                }]),
+            }),
+            // The difference from Negate is that cast allows all inputs.
+            Op::Cast(_) => Cow::Borrowed(&OpDescriptor {
+                commutative: false,
+                inputs: Cow::Borrowed(&[InputDescriptor {
+                    input_kind: InputKind::Data,
+                    denied_primitives: None,
+                }]),
+            }),
+            Op::WriteOutput { .. } | Op::WriteState { .. } => Cow::Borrowed(&OpDescriptor {
+                commutative: false,
+                inputs: Cow::Borrowed(&[InputDescriptor {
+                    input_kind: InputKind::Data,
+                    denied_primitives: None,
+                }]),
+            }),
+            // Difference here is that final inputs are pure dependerncies.
+            Op::Final => Cow::Borrowed(&OpDescriptor {
+                commutative: false,
+                inputs: Cow::Borrowed(&[InputDescriptor {
+                    input_kind: InputKind::PureDependency,
+                    denied_primitives: None,
+                }]),
+            }),
+        }
+    }
 }
